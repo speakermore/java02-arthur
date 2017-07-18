24001f9cbe0898b63b32356e3b0295bec6eec653
Administrator <Administrator@SC-201611301329>
2017-07-18 14:58:32 +0800

    代益铨：修改了arthur.service.user

--- timeattendance/src/arthur/service/user/UserService.java
+++ timeattendance/src/arthur/service/user/UserService.java
@@ -2,39 +2,38 @@ package arthur.service.user;
 
 import org.apache.ibatis.annotations.Param;
 
+import arthur.entity.Student;
+import arthur.entity.Teacher;
+
 public interface UserService {
-	/*
+	/**
 	 * 老师登录
 	 */
-	public User findByTeacherNameAndTeacherPwd(@Param("teacherName") String teacherId,
-			@Param("teacherPwd") String teacherPwd);
+	public Teacher findByTeacherNameAndTeacherPwd(Integer teacherId);
 
 	/**
 	 * 学生登录
 	 */
-	public User findByStudentNameAndStudentPwd(@Param("studentName") String studentId,
-			@Param("studentPwd") String studentPwd);
+	public Student findByStudentNameAndStudentPwd(Integer studentId);
 
 	/**
-	 * 学生注册
+	 * 老师给学生注册
 	 */
-	public Integer addUser(User user);
+	public Integer addTeacher(Integer studentId);
 
 	/**
 	 * 修改学生密码
 	 */
-	public Integer updateUserByStudentName(@Param("studentName") String studentName,
-			@Param("studentPwd") String studentPwd);
+	public Integer updateStudentByStudentName(Integer studentId);
 
 	/**
 	 * 修改老师密码
 	 */
-	public Integer updateUserByTeacherName(@Param("teacherName") String teacherName,
-			@Param("teacherPwd") String teacherPwd);
+	public Integer updateTeacherByTeacherName(Integer teacherId);
 
 	/**
 	 * 根据studentId查询用户（显示学生信息）
 	 */
-	public User selectUserByStudentName(@Param("studentName") Integer studentName);
+	public Student selectUserByStudentName(@Param("studentId") Integer studentId);
 
 }
