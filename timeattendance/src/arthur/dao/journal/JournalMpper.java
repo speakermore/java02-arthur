467aa723740ae5d91fb04a99905412a2051506fc
Administrator <Administrator@SC-201611301329>
2017-07-18 14:32:31 +0800

    代益铨

--- timeattendance/src/arthur/dao/journal/JournalMpper.java
+++
@@ -1,9 +0,0 @@
-package arthur.dao.journal;
-
-import arthur.entity.Journal;
-
-public interface JournalMpper {
-	// 日志填写
-	public Integer addJournal(Journal journal);
-
-}
--- timeattendance/src/arthur/dao/journal/ReplyMpper.java
+++
@@ -1,9 +0,0 @@
-package arthur.dao.journal;
-
-import arthur.entity.Reply;
-
-public interface ReplyMpper {
-	// 问题答复
-	public Integer addReply(Reply reply);
-
-}
